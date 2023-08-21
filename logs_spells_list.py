74ea16078a2f3fd1bc13d69f102aad0639feeefc
Ridepad <ridepad@mail.ru>
2023-08-21 09:09:49 +0300

    1.40.14 - absolete

--- logs_spells_list.py
+++ logs_spells_list.py
@@ -1,22 +1,5 @@
 from constants import LOGGER_UNUSUAL_SPELLS, SPELLS_SCHOOLS, UNUSUAL_SPELLS, running_time
 
-CUSTOM_SPELLS = {
-    "42925": "Flamestrike (Rank 8)",
-    "49240": "Lightning Bolt (Proc)",
-    "49269": "Chain Lightning (Proc)",
-    "53190": "Starfall (AoE)",
-    "55360": "Living Bomb (DoT)",
-    # Off Hand
-    "66974": "Obliterate (Off Hand)",
-    "66962": "Frost Strike (Off Hand)",
-    "61895": "Blood-Caked Strike (Off Hand)",
-    "66992": "Plague Strike (Off Hand)",
-    "44949": "Whirlwind (Off Hand)",
-    "52874": "Fan of Knives (Off Hand)",
-    "57842": "Killing Spree (Off Hand)",
-    "66217": "Rune Strike (Off Hand)",
-}
-
 def spell_id_to_int(data: dict[str, dict[str, str]]):
     return {int(k): v for k,v in data.items()}
 
@@ -46,7 +29,7 @@ def get_all_spells(logs: list[str]):
             _line = line.split(',', 7)
             if _line[6] not in spells:
                 etc = _line[-1].split(',', 2)
-                spells[_line[6]] = {'name': CUSTOM_SPELLS.get(_line[6], etc[0]), 'school': etc[1]}
+                spells[_line[6]] = {'name': etc[0], 'school': etc[1]}
         except IndexError:
             pass
     return finish_spells(spells)
