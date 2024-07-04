5dfed185eb72991886d852b730f3c31026a83616
Ridepad <ridepad@mail.ru>
2024-07-04 18:49:43 +0300

    4.0.30

--- logs_calendar.py
+++ logs_calendar.py
@@ -1,51 +1,32 @@
 import calendar
-import os
 import json
 import shutil
 from collections import defaultdict
-from typing import TypedDict
 from datetime import datetime
 
 import pandas
 import pytz
 
-import file_functions
 import logs_main
-from constants import (
-    DEFAULT_SERVER_NAME,
-    LOGS_DIR,
-    PATH_DIR,
-    UPLOADS_TEXT,
-    get_report_name_info,
-    running_time,
+from constants import DEFAULT_SERVER_NAME
+from c_path import (
+    Directories,
+    PathExt,
 )
+from h_debug import running_time
+from h_other import get_report_name_info
+
 
 CALENDAR = calendar.Calendar()
-
-def get_calend_days(y, m):
-    this_month = CALENDAR.monthdatescalendar(y, m + 1)
-    return [
-        [
-            (cell.strftime("%m-%d"), cell.day)
-            for cell in row
-        ]
-        for row in this_month
-    ]
-
 
 DF_MAIN_NAME = "_logs_list"
 DF_MAIN_EXT = "pkl"
 DF_MAIN_FULL_NAME = f"{DF_MAIN_NAME}.{DF_MAIN_EXT}"
-DF_MAIN_PATH = os.path.join(PATH_DIR, DF_MAIN_FULL_NAME)
 
-class DF_TYPE(TypedDict):
-    df: pandas.DataFrame
-    last_mtime: float
+DF_MAIN_PATH = Directories.main / DF_MAIN_FULL_NAME
+PATH_BKP = Directories.main / f"{DF_MAIN_NAME}.bkp"
+PATH_TMP = Directories.main / f"{DF_MAIN_NAME}.tmp"
 
-DF_MAIN_DATA: DF_TYPE = {
-    "df": pandas.DataFrame(),
-    "last_mtime": 0.0,
-}
 COLUMN_TYPES = {
     "year": "int8",
     "month": "int8",
@@ -57,37 +38,38 @@ COLUMN_TYPES = {
     # "fights": "string",
 }
 
+def get_calend_days(y, m):
+    this_month = CALENDAR.monthdatescalendar(y, m + 1)
+    return [
+        [
+            (cell.strftime("%m-%d"), cell.day)
+            for cell in row
+        ]
+        for row in this_month
+    ]
+
+@DF_MAIN_PATH.cache_until_new_self
 @running_time
-def _read_df(path) -> pandas.DataFrame:
+def _read_df(path: PathExt):
     try:
         return pandas.read_pickle(path)
     except FileNotFoundError:
         return pandas.DataFrame()
+
+def read_main_df() -> pandas.DataFrame:
+    return _read_df()
 
 @running_time
 def _save_df(df: pandas.DataFrame, path, comp=None):
     df.to_pickle(path, compression=comp)
 
 def _save_df_with_backup(df: pandas.DataFrame):
-    PATH_BKP = os.path.join(PATH_DIR, f"{DF_MAIN_NAME}.bkp")
-    PATH_TMP = os.path.join(PATH_DIR, f"{DF_MAIN_NAME}.tmp")
-
-    if os.path.isfile(DF_MAIN_PATH):
+    if DF_MAIN_PATH.is_file():
         shutil.copy2(DF_MAIN_PATH, PATH_BKP)
     _save_df(df, PATH_TMP)
-    
-    DF_MAIN_DATA["df"] = df
-    DF_MAIN_DATA["last_mtime"] = file_functions.get_mtime(PATH_TMP)
-    
-    os.replace(PATH_TMP, DF_MAIN_PATH)
+    PATH_TMP.replace(DF_MAIN_PATH)
 
-
-def get_logs_list_df():
-    current_mtime = file_functions.get_mtime(DF_MAIN_PATH)
-    if current_mtime > DF_MAIN_DATA["last_mtime"]:
-        DF_MAIN_DATA["df"] = _read_df(DF_MAIN_PATH)
-        DF_MAIN_DATA["last_mtime"] = current_mtime
-    return DF_MAIN_DATA["df"]
+##################################################################
 
 def df_filter_by(df: pandas.DataFrame, category: str, filter_by):
     try:
@@ -97,13 +79,20 @@ def df_filter_by(df: pandas.DataFrame, category: str, filter_by):
         return df[df[category].apply(lambda x: filter_by in x)]
 
 def normalize_filter(filter: dict):
-    df = get_logs_list_df()
+    df = read_main_df()
     return {k:v for k,v in filter.items() if v and k in df.columns}
 
 @running_time
 def get_logs_list_df_filter(df: pandas.DataFrame, filter: dict):
-    for f, v in filter.items():
-        df = df_filter_by(df, f, v)
+    print(filter)
+    for filter_key, filter_value in filter.items():
+        # if filter_key == "server":
+        #     df1 = df_filter_by(df, filter_key, filter_value)
+        #     filter_value = filter_value.replace(" ", "-")
+        #     df2 = df_filter_by(df, filter_key, filter_value)
+        #     print(">>>>>>>>>> get_logs_list_df_filter")
+        #     print(filter_value)
+        df = df_filter_by(df, filter_key, filter_value)
         if df.empty:
             break
     return df
@@ -129,12 +118,12 @@ def separate_to_days(df: pandas.DataFrame):
 
 @running_time
 def get_logs_list_filter_json(_filter):
-    df = get_logs_list_df()
+    df = read_main_df()
     df = get_logs_list_df_filter(df, _filter)
     return json.dumps(list(df.index))
 
 def get_logs_list_df_filter_to_calendar_wrap(_filter):
-    df = get_logs_list_df()
+    df = read_main_df()
     if df.empty:
         return {}
     
@@ -143,14 +132,16 @@ def get_logs_list_df_filter_to_calendar_wrap(_filter):
     df.sort_values(by="time", inplace=True)
     return separate_to_days(df)
 
+
+##################################################################
+
 def get_timezone_file(report_id):
-    return os.path.join(UPLOADS_TEXT, f"{report_id}.timezone")
+    return Directories.pending_archive / f"{report_id}.timezone"
 
 def get_timezone(report_id):
-    tz_path = get_timezone_file(report_id)
-    timezone_str = file_functions.file_read(tz_path)
+    timezone = get_timezone_file(report_id).read_text()
     try:
-        return pytz.timezone(timezone_str)
+        return pytz.timezone(timezone)
     except (ValueError, pytz.exceptions.UnknownTimeZoneError):
         return pytz.utc
 
@@ -170,29 +161,36 @@ def convert_timezone(report_id):
         "time": dt.strftime("%H:%M"),
     }
 
-def make_new(folders: list[str]):
-    if not folders:
+def report_data(report_id: str):
+    report = logs_main.THE_LOGS(report_id)
+    date = convert_timezone(report_id)
+    report_name_info = get_report_name_info(report_id)
+    return date | {
+        "author": report_name_info["author"],
+        "server": report_name_info["server"],
+        "player": tuple(report.get_players_guids().values()),
+        "fight": tuple(report.get_enc_data()),
+    }
+
+def make_new(report_ids: list[str]):
+    if not report_ids:
         return
     
     data = {}
-    for report_id in folders:
-        logs_dir = os.path.join(LOGS_DIR, report_id)
-        if not os.path.isdir(logs_dir):
+    for report_id in report_ids:
+        logs_dir = Directories.logs / report_id
+        needed_files = (
+            file_path.is_file()
+            for file_path in [
+                logs_dir / "PLAYERS_DATA.json",
+                logs_dir / "ENCOUNTER_DATA.json",
+            ]
+        )
+        if not all(needed_files):
             continue
-        report = logs_main.THE_LOGS(report_id)
-        if not os.path.isfile(report.relative_path("PLAYERS_DATA.json")):
-            continue
-        if not os.path.isfile(report.relative_path("ENCOUNTER_DATA.json")):
-            continue
+
         try:
-            date = convert_timezone(report_id)
-            report_name_info = get_report_name_info(report_id)
-            data[report_id] = date | {
-                "author": report_name_info["author"],
-                "server": report_name_info["server"],
-                "player": tuple(report.get_players_guids().values()),
-                "fight": tuple(report.get_enc_data()),
-            }
+            data[report_id] = report_data(report_id)
         except Exception:
             continue
     
@@ -207,25 +205,25 @@ def _get_default_server(name: str):
 
 def add_new_logs(new_reports: list[str]=None):
     if new_reports is None:
-        new_reports = os.listdir(LOGS_DIR)
+        new_reports = Directories.logs.iterdir()
     new_reports = set(new_reports)
     
-    df = get_logs_list_df()
+    df = read_main_df()
     df_index = set(df.index)
     
-    default_copies = {
+    reports_copies_with_default_name = {
         _get_default_server(report_id)
         for report_id in new_reports
         if DEFAULT_SERVER_NAME not in report_id
     }
-    default_copies = default_copies & df_index
+    reports_copies_with_default_name = reports_copies_with_default_name & df_index
 
     folders = new_reports - df_index
     new_df = make_new(folders)
-    if new_df is None and not default_copies:
+    if new_df is None and not reports_copies_with_default_name:
         return
     
-    df.drop(default_copies, inplace=True)
+    df.drop(reports_copies_with_default_name, inplace=True)
     df = pandas.concat([df, new_df])
     df.sort_index()
     _save_df_with_backup(df)
