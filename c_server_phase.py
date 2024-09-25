eb84f4b26f78737a8496db5c7b9db2b82fee2928
Ridepad <ridepad@mail.ru>
2024-09-25 03:35:11 +0300

    6.11.0 - t8

--- c_server_phase.py
+++ c_server_phase.py
@@ -116,6 +116,30 @@ class Tier_7(Tier_3):
     )
     ALL_BOSSES = Tier_3.FOR_POINTS + OTHER
 
+class Tier_8:
+    BOSSES_GET_GUID_NAME_PAIRS_FROM = (
+        Encounter("XT-002 Deconstructor", "25N"),
+    )
+    FOR_POINTS = (
+        Encounter("Ignis the Furnace Master", "25N"),
+        Encounter("Razorscale", "25N"),
+        Encounter("XT-002 Deconstructor", "25N"),
+        Encounter("Assembly of Iron", "25N"),
+        Encounter("Kologarn", "25N"),
+        Encounter("Auriaya", "25N"),
+        Encounter("Hodir", "25N"),
+        Encounter("Thorim", "25N"),
+        Encounter("Freya", "25N"),
+        Encounter("Mimiron", "25N"),
+        Encounter("General Vezax", "25N"),
+        Encounter("Yogg-Saron", "25N"),
+        Encounter("Algalon the Observer", "25N"),
+    )
+    OTHER = (
+        Encounter("Emalon the Storm Watcher", "25N"),
+    )
+    ALL_BOSSES = FOR_POINTS + OTHER
+
 
 SERVER_PHASE: dict[str, str] = Files.server_phases.json_ignore_error()
 
