30195a4c2e1f462e6f2e983d6e49dc75946509d7
Ridepad <ridepad@mail.ru>
2024-11-04 06:24:54 +0200

    6.18.1

--- top_gear.py
+++ top_gear.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from api_db import DB, Table
 from c_path import Directories, PathExt, StrEnum
-from h_debug import Loggers, running_time
+from h_debug import Loggers
 
 LOGGER_GEAR_PARSER = Loggers.gear
 NO_DATA = (0, gzip.compress(b"{}"))
@@ -40,16 +40,16 @@ class CharGear:
     def __init__(
         self,
         name: str,
-        server: str,
-        last_modified: int,
         data: bytes,
+        server: str="Unknown",
+        last_modified: int=0,
     ) -> None:
         self.name = name
         self.server = server
-        self.last_modified = last_modified
         self.data = data
         self.size = len(data)
         self.size_compressed = self.size
+        self.last_modified = last_modified
 
     @property
     def gear_dict(self) -> dict:
@@ -65,15 +65,24 @@ class CharGear:
     def gear_id(self):
         return f"{self.server}--{hash(self.name)}--{self.last_modified}"
     
-    def add_new_gear_snippet(self, new_profile: dict):
-        if not new_profile:
-            LOGGER_GEAR_PARSER.debug(f"{self.name:12} | parse_profile | not new_profile")
-            return
-        
+    def as_db_row(self):
+        jd = json.dumps(self.gear_dict, separators=(',', ':'))
+        jdb = jd.encode()
+        jdbgzip = gzip.compress(jdb)
+        return (
+            self.name,
+            int(next(reversed(self.gear_dict))),
+            jdbgzip,
+        )
+    
+    def update(self, new_profile: dict):
         if self.is_same_as_last_recorded(new_profile):
             LOGGER_GEAR_PARSER.debug(f"{self.name:12} | parse_profile | same as before")
-            return
-        
+            return False
+        self.add_new_gear_set(new_profile)
+        return True
+    
+    def add_new_gear_set(self, new_profile: dict):
         timestamp_now = int(datetime.now().timestamp())
         for timestamp, profile in self.gear_dict.items():
             if profile == new_profile:
@@ -81,14 +90,14 @@ class CharGear:
                 return
 
         self.gear_dict[timestamp_now] = new_profile
-        return True
     
     def is_same_as_last_recorded(self, new_profile: dict):
-        try:
-            last_profile = next(reversed(self.gear_dict.values()))
-        except StopIteration:
+        if not new_profile:
+            return False
+        if not self.gear_dict:
             return False
         
+        last_profile = next(reversed(self.gear_dict.values()))
         if not isinstance(last_profile, dict):
             last_profile = self.gear_dict.get(last_profile)
         
@@ -118,7 +127,7 @@ class GearDB(DB):
         path = Directories.gear / f"{server}.db"
         super().__init__(path, new, without_row_id=True)
         self.server = server
-        self.table = TableGear(server)
+        self.table = TableGear()
 
     def get_player_data(self, player_name: str):
         query = self.table.player_query(player_name)
@@ -127,44 +136,59 @@ class GearDB(DB):
         except TypeError:
             LAST_MODIFIED, DATA = NO_DATA
         except Exception:
-            LOGGER_GEAR_PARSER.exception(f"{player_name:12} | get_player_data")
+            # LOGGER_GEAR_PARSER.exception(f"{player_name:12} | get_player_data")
             LAST_MODIFIED, DATA = NO_DATA
 
         return CharGear(
             name=player_name,
+            data=DATA,
             server=self.server,
             last_modified=LAST_MODIFIED,
-            data=DATA,
         )
 
-    @running_time
-    def get_player_data_dict(self, player_name: str):
-        character = self.get_player_data(player_name)
-        return character.gear_dict
-
-    def update_player(self, player_name: str, new_profile: dict):
+    def update_player_row(self, player_name: str, new_profile: dict):
         if not new_profile:
             LOGGER_GEAR_PARSER.debug(f"{player_name:12} | parse_profile | not new_profile")
             return
         
-        player_profile = self.get_player_data_dict(player_name)
-        if is_same_as_last_recorded(player_profile, new_profile):
-            LOGGER_GEAR_PARSER.debug(f"{player_name:12} | parse_profile | same as before")
+        character = self.get_player_data(player_name)
+        if not character.update(new_profile):
             return
 
-        add_new_gear_set(player_profile, new_profile)
-
         rows = (
-            new_db_row(player_name, player_profile),
+            character.as_db_row(),
         )
         self.add_new_rows(self.table, rows)
-        LOGGER_GEAR_PARSER.debug(f"{player_name:12} | parse_profile | added")
+        # LOGGER_GEAR_PARSER.debug(f"{player_name:12} | parse_profile | added")
         return True
-
+    
+    def update_players_rows(self, player_new_data: dict[str, dict]):
+        rows = []
+        for character in self._gen_players_rows(player_new_data):
+            new_profile = player_new_data[character.name]
+            if not character.update(new_profile):
+                continue
+            
+            rows.append(character.as_db_row())
+        
+        self.add_new_rows(self.table, rows)
+    
+    # def _gen_players_rows(self, player_names: list[str]):
+    #     q = self.table.player_query_list(player_names)
+    #     for name, timestamp, data in self.cursor.execute(q):
+    #         yield CharGear(
+    #             name=name,
+    #             data=data,
+    #             server=self.server,
+    #             last_modified=timestamp,
+    #         )
+    def _gen_players_rows(self, player_names: list[str]):
+        for player_name in player_names:
+            yield self.get_player_data(player_name)
 
 def new_db_row(name: str, data: dict):
     if not data:
-        LOGGER_GEAR_PARSER.debug(f"{name:12} | new_db_row | not new_profile")
+        # LOGGER_GEAR_PARSER.debug(f"{name:12} | new_db_row | not new_profile")
         return
     
     jd = json.dumps(data, separators=(',', ':'))
@@ -172,19 +196,14 @@ def new_db_row(name: str, data: dict):
     jdbgzip = gzip.compress(jdb)
     return name, int(next(reversed(data))), jdbgzip
 
-def new_db_row_wrap_file(fpath: PathExt):
-    data = fpath.read_bytes()
-    try:
-        data_dict = json.loads(data)
-    except Exception:
-        return
-    return new_db_row(fpath.stem, data_dict)
-
 def test1():
     server = "Lordaeron"
     db = GearDB(server)
-    # z = db.get_player_data_dict("Nomadra")
-    z = db.get_player_data_dict("Nyaffliction")
+    z = db.get_player_data("Nyaffliction").gear_dict
+    print(z.keys())
+    server = "WoW-Circle-x100"
+    db = GearDB(server)
+    z = db.get_player_data("Уродец").gear_dict
     print(z.keys())
     # server = "Icecrown"
     # db = GearDB(server)
@@ -198,10 +217,10 @@ def test2():
     d = list(fj.values())[-1]
 
     db = GearDB("Lordaeron")
-    s = db.update_player("Nyaffliction", d)
+    s = db.update_player_row("Nyaffliction", d)
     print(s)
 
 if __name__ == "__main__":
     # _convert()
-    test2()
+    # test2()
     test1()
